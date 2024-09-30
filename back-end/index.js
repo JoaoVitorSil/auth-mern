924d02a7ee1b642fd1749d1edd290c41079c4e1c
JoaoVitorSil <joaovitorsil13@gmail.com>
2024-09-30 20:19:59 -0300

    dksajdk

--- back-end/index.js
+++ back-end/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, 'front-end/build')));
+    app.use(express.static(path.join(__dirname, 'front-end/dir')));
     app.get("*", (req,res) =>{
         res.sendFile(path.resolve(__dirname, "front-end", "dist", "index.html"));
     })
